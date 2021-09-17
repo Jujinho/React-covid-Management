1d1e4bf58aca7b74b4c29cf8645cfba7a328bb5b
Jujinho <68211902+Jujinho@users.noreply.github.com>
2021-09-18 06:28:48 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
+const customers = [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name': 'Ju Jinho'
+  ,'birthday': '001026'
+  ,'gender': 'Male'
+  ,'job': 'Student'
+  
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name': 'James'
+  ,'birthday': '981021'
+  ,'gender': 'Male'
+  ,'job': 'professor'
+  
+
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name': 'Tuan'
+  ,'birthday': '960514'
+  ,'gender': 'Male'
+  ,'job': 'Designer'
+  
+
+}
+
+]
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+
+          )
+        })
+      }
       
     </div>
-  );
+          
+    
+      );
+
+  }
 }
 
 export default App;
